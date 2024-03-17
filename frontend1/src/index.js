0ec24ea0c36a0cff1db86f72a717f953fad358c7
thirumurugan7 <thiru82003@gmail.com>
2024-03-18 01:33:14 +0530

    changed project id

--- frontend1/src/index.js
+++ frontend1/src/index.js
@@ -11,9 +11,9 @@ root.render(
   <>
     <WalletProvider
       enableHostedBackups={true}
-      collectAndReportMetrics
-      prod={false}
-      projectId={"0f9bcc25-9ab2-42b5-90d2-122588e83383"}
+      collectAndReportMetrics={true}
+      prod={true}
+      projectId={"a6ff9b4c-9586-4920-a38f-6a77bf39a375"}
     >
       <Provider store={store}>
         <App />

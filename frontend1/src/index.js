c1882fd66130c6c1a33d428f04e0c3015ea3cfc5
S.THIRUMURUGAN <67679146+Thirumurugan7@users.noreply.github.com>
2024-03-27 19:59:11 +0530

    Update index.js

--- frontend1/src/index.js
+++ frontend1/src/index.js
@@ -11,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <WalletProvider
+verbose={true}
       enableHostedBackups={true}
       collectAndReportMetrics
       prod={false}

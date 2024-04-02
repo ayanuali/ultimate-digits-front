83173f14628816ceaf444cd7bb2fe54e480d3c89
thirumurugan sivalingam <thiru@synlian.net>
2024-04-02 18:14:58 +0530

    changing coinlayer api

--- frontend1/src/index.js
+++ frontend1/src/index.js
@@ -8,6 +8,9 @@ import { WalletProvider, useWalletContext } from "@coinbase/waas-sdk-web-react";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//localhost: 0f9bcc25-9ab2-42b5-90d2-122588e83383
+//beta: a6ff9b4c-9586-4920-a38f-6a77bf39a375
 root.render(
   <>
     <WalletProvider
